baeb470bdab73fc79b51dfa8a8394672fb3adcf2
Tiggax <66419530+Tiggax@users.noreply.github.com>
2020-12-18 11:36:22 +0100

    Create matrike.c
    kako se definirajo matrike

---
+++ matrike.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int matrika[3]={3,4,5};// matrike se definira z bracketi.
+  for(int i=0; i < 3 ; i++){
+    printf("\nelement z index %i je %i\n",i, matrika[i]);
+    size_t
+  }
+  int vecjamatrika[2][3]={{1,2,3},{4,5,6}};// matrike se lahko definira tudi kot dvodimenzojonalne(kot so matrike), pri tem se 
+                                           //definira s prvo vrednostjo( v tem primeru 2) koliko Arrayov bo, s drugo vrednostjo
+                                           //pa se definira koliko elementov bo v posameznem arrayu. predstavitev->
+                                           //  vecjamatrika=2x(matrika[3])
+  return 0;
+}
